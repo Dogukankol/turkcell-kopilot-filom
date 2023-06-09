2ce47d618b2bdddac8093b6a23f5e3822aaa164d
Doğukan Kol <43730866+Dogukankol@users.noreply.github.com>
2023-06-10 00:41:32 +0300

    layout changes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,4 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '@/components/header'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
