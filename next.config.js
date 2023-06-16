c153ad314b7a75552902ccd64dbb91f805c78b68
Doğukan Kol <43730866+Dogukankol@users.noreply.github.com>
2023-06-16 15:11:23 +0300

    add nextconfig dummyjson.com

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build'
+    distDir: 'build',
+    experimental: { appDir: true },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'i.dummyjson.com',
+            port: '',
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig
